e1218f107fe91753b03622049d652bdcdf5408a8
Bugra Yilmaz <bugra@promaton.com>
2024-10-01 08:36:28 +0000

    fix argument ordering - torch-geometric upgrade requires this

--- torch_points3d/core/data_transform/grid_transform.py
+++ torch_points3d/core/data_transform/grid_transform.py
@@ -116,9 +116,9 @@ class GridSampling3D:
 
         coords = torch.round((data.pos) / self._grid_size)
         if "batch" not in data:
-            cluster = grid_cluster(coords, torch.tensor([1, 1, 1]))
+            cluster = grid_cluster(coords, None, torch.tensor([1, 1, 1]))
         else:
-            cluster = voxel_grid(coords, data.batch, 1)
+            cluster = voxel_grid(coords, 1, data.batch)
         cluster, unique_pos_indices = consecutive_cluster(cluster)
 
         data = group_data(data, cluster, unique_pos_indices, mode=self._mode)
